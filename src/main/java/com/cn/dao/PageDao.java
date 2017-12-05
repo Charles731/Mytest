bf5b106c9e1f32c1e4b3ef3c386367596aa82095
e <871045396@qq.com>
2017-12-05 11:05:42 +0800

    12.05 通过分页插件PageHelper实现分页功能

--- src/main/java/com/cn/dao/PageDao.java
+++ src/main/java/com/cn/dao/PageDao.java
@@ -1,10 +1,7 @@
 package com.cn.dao;
 
 import com.cn.entity.User;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by dellpc on 2017/11/30.
